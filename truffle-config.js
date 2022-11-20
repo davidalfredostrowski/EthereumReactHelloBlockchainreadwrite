e19f16e61f382b83d69d05f60ee5f7ec768e7d1e
Ubuntu <ubuntu@ip-172-31-48-169.us-west-2.compute.internal>
2022-11-20 04:15:31 +0000

    test ^O
    ^X
    ZZZ
    
    ZZZZZZZZZZZZ

--- truffle-config.js
+++ truffle-config.js
@@ -2,7 +2,7 @@ module.exports = {
   migrations_directory: "./migrations",
   networks: {
     development: {
-      host: "ec2-18-246-4-84.us-west-2.compute.amazonaws.com",
+      host: "ec2-35-83-253-13.us-west-2.compute.amazonaws.com",
       port: 8545,
       network_id: "*" // Match any network id
     }
